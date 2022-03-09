dd96865e60fc74ca605ee785e3f5273b3a198894
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-03-09 14:37:36 -0600

    Added a quick navbar with Zip code input. Added event listener to JS which appends brewery API url with zipcode input.

--- Assests/JS/breweries-script.js
+++ Assests/JS/breweries-script.js
@@ -1,28 +1,46 @@
-var tableBody = document.getElementById('breweryTable');
+var tableBody = document.getElementById("breweryTable");
+var buttonClick = document.getElementById("submitButton");
+var zipCodeVariable = "";
 
 // Validated API connection to https://www.openbrewerydb.org/ using a static zipcode
 function openBreweriesApi() {
-  var openBreweryDBUrl =
-    "https://api.openbrewerydb.org/breweries?by_postal=53212";
+  zipCodeVariable = document.getElementById("zipCode").value;
+  console.log(zipCodeVariable);
+  var openBreweryDBUrl = "https://api.openbrewerydb.org/breweries?by_postal=";
+
+  openBreweryDBUrl += zipCodeVariable;
+
+  console.log(openBreweryDBUrl);
 
   fetch(openBreweryDBUrl)
     .then(function (response) {
       return response.json();
     })
     // loop to add data to table
+
+    // document.getElementById("breweryTable").empty();
+
     .then(function (data) {
       console.log(data);
       for (var i = 0; i < data.length; i++) {
         let template = `
                         <tr>
-                            <td>${data[i].name}</td>
-                            <td>${data[i].brewery_type}</td>
-                            <td>${data[i].state}</td>
+                            <td class="w-1/2 py-2 border-2 border-teal-200 bg-slate-100">${data[i].name}</td>
+                            <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${data[i].brewery_type}</td>
+                            <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${data[i].state}</td>
                         <tr>
-                        `
+                        `;
         tableBody.innerHTML += template;
       }
     });
 }
 
-openBreweriesApi();
+buttonClick.addEventListener("click", openBreweriesApi);
+
+// Starting on submit function when the enter key is hit while in the Zip Code area
+// zipCode.addEventListener("keyup", (event) => {
+//   if(event.keyCode === 13); {
+//     event.preventDefault();
+//     console.log("enter hit")
+//   }
+// });
