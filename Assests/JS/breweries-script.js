d7da4cfc0449f3576e3a7d7bc9f66ff2867ed0f5
Miguel Escobar <mae2136@gmail.com>
2022-03-09 20:11:08 -0600

    index.html: - Add taco table display. - Fix formatting. - Remove extra lines 11 and 12.
    breweries-script.js:
    - Add lat and lon test variables.
    - Add documenu API for Mexican food.
    - Add fetch for documenu with test variables.

--- Assests/JS/breweries-script.js
+++ Assests/JS/breweries-script.js
@@ -1,12 +1,21 @@
 var tableBody = document.getElementById("breweryTable");
+var tacoTable = document.getElementById("tacoTable")
 var buttonClick = document.getElementById("submitButton");
+// Documenu Geo API
+var documenuApi = "https://api.documenu.com/v2/restaurants/search/geo?key=442e928049c9bb7b553d48b27474017a&cuisine=Mexican&distance=2&"
 var zipCodeVariable = "";
+// Test Variables
+var lat = 41.92;
+var lon = -87.76;
 
 // Validated API connection to https://www.openbrewerydb.org/ using a static zipcode
 function openBreweriesApi() {
   zipCodeVariable = document.getElementById("zipCode").value;
   console.log(zipCodeVariable);
   var openBreweryDBUrl = "https://api.openbrewerydb.org/breweries?by_postal=";
+
+  var documenuUrl = documenuApi + "lat=" + lat + "&lon=" + lon;
+  console.log(documenuUrl)
 
   openBreweryDBUrl += zipCodeVariable;
 
@@ -31,6 +40,23 @@ function openBreweriesApi() {
                         <tr>
                         `;
         tableBody.innerHTML += template;
+      }
+    });
+  fetch(documenuUrl)
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function (tacoData) {
+      console.log(tacoData);
+      for (var i = 0; i < tacoData.data.length; i++) {
+        let template = `
+                        <tr>
+                            <td class="w-1/2 py-2 border-2 border-teal-200 bg-slate-100">${tacoData.data[i].restaurant_name}</td>
+                            <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${tacoData.data[i].address.state}</td>
+                        <tr>
+                        `;
+        tacoTable.innerHTML += template;
       }
     });
 }
