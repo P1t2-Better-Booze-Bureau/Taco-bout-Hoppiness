daed3b03a54fafa75d59598789918c802ef098ce
Youssef Kirollos <ykirollos89@gmail.com>
2022-03-10 21:10:35 -0600

    Co-authored-by: Miguel Escobar <mae2136@users.noreply.github.com> Co-authored-by: JesseComeau <JesseComeau@users.noreply.github.com> Co-authored-by: GlennJohnYC <GlennJohnYC@users.noreply.github.com>

--- Assests/JS/breweries-script.js
+++ Assests/JS/breweries-script.js
@@ -1,39 +1,31 @@
 var tableBody = document.getElementById("breweryTable");
-var tacoTable = document.getElementById("tacoTable")
+var tacoTable = document.getElementById("tacoTable");
 var buttonClick = document.getElementById("submitButton");
-// Documenu Geo API
-var documenuApi = "https://api.documenu.com/v2/restaurants/search/geo?key=442e928049c9bb7b553d48b27474017a&cuisine=Mexican&distance=2&fullmenu"
+// Documenu Geo API / Needs fresh key for demonstration
+var documenuApi =
+  "https://api.documenu.com/v2/restaurants/search/geo?key=1d72ed71331751f36558c92ff7f8a0cf&cuisine=Mexican&distance=2&fullmenu";
+  // additional key: 442e928049c9bb7b553d48b27474017a
 var zipCodeVariable = "";
 // Test Variables
-var lat = 41.92;
-var lon = -87.76;
-// Validated API connection to https://www.openbrewerydb.org/ using a static zipcode
-function openBreweriesApi() {
-  zipCodeVariable = document.getElementById("zipCode").value;
-  console.log(zipCodeVariable);
-  var openBreweryDBUrl = "https://api.openbrewerydb.org/breweries?by_postal=";
-  var openWeatherApi = `https://api.openweathermap.org/geo/1.0/zip?appid=35e74e9930a9378bb724ee6a8d2a3427&zip=${zipCodeVariable},US`;
-  console.log(openWeatherApi)
-  
-  //zip to long-lat
- // fetch(openWeatherApi)
-  //.then(function (response) {
-   // console.log(response)
-    //return response.json();
-  //})
-  
-  var documenuUrl = documenuApi + "&lat=" + lat + "&lon=" + lon;
-  console.log(documenuUrl)
+var coordinate = [ ];
+// Get hoppiness button event listener
+buttonClick.addEventListener("click", geocode);
 
-  openBreweryDBUrl += zipCodeVariable;
+// Brewery card selected event listener
+
+// Function to pull brewery information based on geolocation
+function findHoppiness(coordinate) {
+  var openBreweryDBUrl =
+    "https://api.openbrewerydb.org/breweries?per_page=5&by_dist=";
+
+  openBreweryDBUrl += `${coordinate[0]},${coordinate[1]}`;
 
   console.log(openBreweryDBUrl);
 
   fetch(openBreweryDBUrl)
     .then(function (response) {
-      console.log(response)
+      console.log(response);
       return response.json();
-
     })
     // loop to add data to table
 
@@ -43,15 +35,57 @@ function openBreweriesApi() {
       console.log(data);
       for (var i = 0; i < data.length; i++) {
         let template = `
-                        <tr>
+      <tr>
                             <td class="w-1/2 py-2 border-2 border-teal-200 bg-slate-100">${data[i].name}</td>
                             <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${data[i].brewery_type}</td>
                             <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${data[i].state}</td>
-                        <tr>
-                        `;
+                            <tr>
+                            `;
         tableBody.innerHTML += template;
       }
     });
+}
+
+// Starting on submit function when the enter key is hit while in the Zip Code area
+// zipCode.addEventListener("keyup", (event) => {
+//   if(event.keyCode === 13); {
+//     event.preventDefault();
+//     console.log("enter hit")
+//   }
+// });
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 8,
+//     center: { lat: -95.7129, lng: 37.0902 },
+//     mapTypeControl: false,
+//   });
+// }
+
+// Function to pull geolocation from user zip code
+function geocode() {
+  zipCodeVariable = document.getElementById("zipCode").value;
+  var openWeatherApi = `http://api.openweathermap.org/geo/1.0/zip?appid=d20682e8d5e2100a9c4e1e2f42e32a85&zip=${zipCodeVariable},US`;
+ fetch(openWeatherApi)
+ .then(function(response)
+ {
+   return response.json();
+ })
+
+.then(function(data){
+  console.log(data, data.lat, data.lon);
+  coordinate=[data.lat, data.lon];
+  console.log(coordinate);
+  findHoppiness(coordinate);
+  getTaco(coordinate);
+})
+};
+
+// Function to pull Mexican food from geolocation
+function getTaco(coordinate) {
+  var documenuUrl = `${documenuApi}&lat=${coordinate[0]}&lon=${coordinate[1]}`;
+  console.log(documenuUrl);
+
   fetch(documenuUrl)
     .then(function (response) {
       return response.json();
@@ -62,21 +96,11 @@ function openBreweriesApi() {
       for (var i = 0; i < tacoData.data.length; i++) {
         let template = `
                         <tr>
-                            <td class="w-1/2 py-2 border-2 border-teal-200 bg-slate-100">${tacoData.data[i].restaurant_name}</td>
-                            <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${tacoData.data[i].address.state}</td>
+                        <td class="w-1/2 py-2 border-2 border-teal-200 bg-slate-100">${tacoData.data[i].restaurant_name}</td>
+                        <td class="w-1/4 py-2 border-2 border-teal-200 bg-slate-100">${tacoData.data[i].address.state}</td>
                         <tr>
                         `;
         tacoTable.innerHTML += template;
       }
     });
 }
-
-buttonClick.addEventListener("click", openBreweriesApi);
-
-// Starting on submit function when the enter key is hit while in the Zip Code area
-// zipCode.addEventListener("keyup", (event) => {
-//   if(event.keyCode === 13); {
-//     event.preventDefault();
-//     console.log("enter hit")
-//   }
-// });
