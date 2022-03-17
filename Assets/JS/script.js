4ef3ed0ded318a1870cada2828ece88a2a9a4169
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-03-17 18:03:15 -0500

    Removed CSS styles and used Tailwind class names. Fromatted javascript for easier viewing. Changed view of scroll buttons and functionality. Made eneter key functionality work in input section.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,17 +4,17 @@ var tacosRight = document.querySelector("#tacoButtonRight");
 var tacosLeft = document.querySelector("#tacoButtonLeft");
       
 breweriesRight.addEventListener("click", function(event){
-document.querySelector("#breweryTable").scrollLeft += 640;
+document.querySelector("#breweryTable").scrollLeft += 924;
 })
 
 breweriesLeft.addEventListener("click", function(event){
-document.querySelector("#breweryTable").scrollLeft -= 640;
+document.querySelector("#breweryTable").scrollLeft -= 924;
 })
 
 tacosRight.addEventListener("click", function(event){
-document.querySelector("#tacoTable").scrollLeft += 640;
+document.querySelector("#tacoTable").scrollLeft += 924;
 })
 
 tacosLeft.addEventListener("click", function(event){
-document.querySelector("#tacoTable").scrollLeft -= 640;
+document.querySelector("#tacoTable").scrollLeft -= 924;
 })
