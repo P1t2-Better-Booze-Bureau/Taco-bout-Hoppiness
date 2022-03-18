164102c19b8c52a5d5a10d7c756a48f5bd0dcf69
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-03-18 18:30:03 -0500

    Favorites page with JS

--- Assets/JS/favorites.js
+++ Assets/JS/favorites.js
@@ -1,36 +1,103 @@
-// var tacoData = localStorage.getItem("mytime");
+var savedFavorites = JSON.parse(localStorage.favorites)
+var favoriteTacos = savedFavorites.tacos;
+var favoriteBreweries = savedFavorites.brewery;
+var favoritesTable = document.getElementById("favoritesTable");
 
-console.log(localStorage)
-console.log(localStorage.tacoTest)
+function websiteAdd(data) {
+  var removeURL = data.replace(/^https?:\/\//, "");
+  // Figure out how to wrap website text when smaller screen size
+  let websiteTemplate = `
+  <a href=${data} target="_blank" class="object-cover text-Indigo-900 text-lg font-semibold no-underline hover:decoration-2 hover:underline">${removeURL}</a>`;
+  return websiteTemplate;
+}
 
-var tacoData = localStorage.getItem("tacoTest");
-var tacoData2 = JSON.parse(tacoData)
+function populateFavorites () {
+    for (var i = 0; i < favoriteTacos.length; i++) {
 
-console.log(tacoData);
-console.log(tacoData2);
-// function Parse(tacoData) {
-//     return tacoData.json();}
-    
-// console.log(data);
+      // Brewery Cards
+      let template = `<tr class="bg-white bg-opacity-75">
 
+                        <td class="py-4 px-6 text-sm border-2 font-medium text-gray-900 whitespace-nowrap">  
+                             
+                          <div class="rounded-2xl overflow-hidden shadow-lg py-2.5 px-2.5 text-left justify-between hover:scale-105 z-10 bg-cover bg-[url('./Assets/pictures/BeerSuds.jpg')]">
+                  
+                            <div class="font-extrabold text-xl h-1/4 mb-2">${favoriteBreweries[i].name}</div>
 
-// var taco = JSON.parse(localStorageTest.tacoTest)
-// console.log(taco)
+                            <p class="text-black font-semibold text-left text-lg"> Address: <br>
+                            ${favoriteBreweries[i].street} <br>
+                            ${favoriteBreweries[i].city}, ${favoriteBreweries[i].state}
+                            ${favoriteBreweries[i].postal_code.substring(0,5)}</p><br>`;
+                              if (favoriteBreweries[i].website_url != null) {
+                                template += websiteAdd(favoriteBreweries[i].website_url);
+                              } else {
+                                template += `<br>`
+                              }
+        // Taco Cards
+        template +=  `
+                            </div>                          
+                        </td>                          
 
-function populateFavorites (tacoData2) {
-    for (var i = 0; i < data.length; i++) {
-      let template = `
-      <div class="rounded overflow-hidden shadow-lg flex flex-shrink-0 min-w-1/4 content-between hover:scale-105 bg-indigo-300 hover:bg-indigo-400 bungee font-bold bg-[url('./Assets/pictures/Tacos.jpg')]">
-    
-        <div class="py-2.5 px-2.5 text-left justify-between hover:bg-indigo-400 hover:bg-opacity-50 tacoCard">
-         <div class="font-bold text-xl mb-2">${localStorageTest.data[i].restaurant_name} </div>
-         <p class="text-black font-semibold text-left text-base class="bg-amber-700 text-slate-100 text-base"> Address: ${localStorageTest.data[i].address.formatted}</p><br>`;
-         
-         if (localStorageTest.data[i].restaurant_website != null) {
-        tacoCardTemplate += websiteAdd(
-          localStorageTest.data[i].restaurant_website
-        );
-         }
+                        <td class="py-4 px-6 text-sm border-2 font-medium text-gray-900 whitespace-nowrap">
+                                                   
+                          <div class="rounded-2xl overflow-hidden shadow-lg py-2.5 px-2.5 text-left justify-between text-left hover:scale-105 z-10 bg-cover bg-[url('./Assets/pictures/Tacos.jpg')]">
+
+                            <div class="font-extrabold text-xl h-1/4 mb-2">${favoriteTacos[i].restaurant_name}</div>
+                          
+                            <p class="text-black font-semibold text-left text-lg"> 
+                              Address: <br>
+                              ${favoriteTacos[i].address.street} <br>
+                              ${favoriteTacos[i].address.city}, ${favoriteTacos[i].address.state}
+                              ${favoriteTacos[i].address.postal_code}
+                            </p><br>`;
+                              
+                              if (favoriteTacos[i].restaurant_website != "") {
+                              template += websiteAdd(favoriteTacos[i].restaurant_website);
+                              } else {
+                                template += `<br>`
+                              }
         
+        // Delete Button
+        template += `
+            </div>
+          </td>
+          <td class="py-4 px-6 text-sm font-medium border-2 text-right whitespace-nowrap">
+            <input type="button"
+              class="text-blue-600 hover:underline deleteButton" value="Delete" name=${i} onclick="deleteButton()">
+            <div class="invisible h-1" id="rowIndex">${i}</div>
+          
+            </a>
+          </td>
+        </tr>`
+                            
+        
+        
+        favoritesTable.innerHTML += template;
+         }
+        }
+         populateFavorites();
 
-      tacoTable.innerHTML += tacoCardTemplate;
+var rowIndex = "";
+
+
+
+function deleteButton() {
+  rowIndex = event.target.name;
+  
+  var deleteBrewery = savedFavorites.brewery[rowIndex];
+  var deleteTaco = savedFavorites.tacos[rowIndex];
+  
+  deleteBrewery = JSON.stringify(deleteBrewery);
+  deleteTaco = JSON.stringify(deleteTaco);
+  
+  var localStorageVar = localStorage.favorites
+  console.log(localStorageVar)
+  
+  localStorageVar = localStorageVar.replace(deleteBrewery,'')
+  localStorageVar = localStorageVar.replace(deleteTaco,'')
+  console.log(localStorageVar)
+
+  localStorage.setItem('favorites',localStorageVar);
+
+  populateFavorites();
+
+}
